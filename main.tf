f7f87a945c486c1d36881d36f8680271d67bb3b2
Bartosz Mitmanski <bartosz.mitmanski@iongroup.com>
2024-11-06 11:18:39 +0100

    CSIXREV-6554 | BM BS | add idle_timeout

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
 module "404_ecs_service" {
   source = "modules/deprecated"
 
-  name   = format("%s-%s-404", var.env, var.component)
-  vpc_id = var.platform_config["vpc"]
+  name                 = "${format("%s-%s-404", var.env, var.component)}"
+  vpc_id               = "${var.platform_config["vpc"]}"
 }
 
 module "alb" {
   source = "github.com/mergermarket/tf_alb"
 
-  name                     = format("%s-%s-router", var.env, var.component)
-  vpc_id                   = var.platform_config["vpc"]
+  name                     = "${format("%s-%s-router", var.env, var.component)}"
+  vpc_id                   = "${var.platform_config["vpc"]}"
   subnet_ids               = ["${split(",", var.alb_internal ? var.platform_config["private_subnets"] : var.platform_config["public_subnets"])}"]
-  internal                 = var.alb_internal
-  extra_security_groups    = concat(list(var.platform_config["ecs_cluster.default.client_security_group"]), var.extra_security_groups)
-  certificate_domain_name  = format("*.%s%s", var.env != "live" ? "dev." : "", var.dns_domain)
-  default_target_group_arn = aws_alb_target_group.default_target_group.arn
-  access_logs_bucket       = lookup(var.platform_config, "elb_access_logs_bucket", "")
-  access_logs_enabled      = "${lookup(var.platform_config, "elb_access_logs_bucket", "")}" == "" ? false : true
-  idle_timeout             = var.idle_timeout
+  internal                 = "${var.alb_internal}"
+  extra_security_groups    = "${concat(list(var.platform_config["ecs_cluster.default.client_security_group"]), var.extra_security_groups)}"
+  certificate_domain_name  = "${format("*.%s%s", var.env != "live" ? "dev." : "", var.dns_domain)}"
+  default_target_group_arn = "${aws_alb_target_group.default_target_group.arn}"
+  access_logs_bucket       = "${lookup(var.platform_config, "elb_access_logs_bucket", "")}"
+  access_logs_enabled      = "${"${lookup(var.platform_config, "elb_access_logs_bucket", "")}" == "" ? false : true}"
+  idle_timeout             = "${var.idle_timeout}"
 
   tags = {
     component   = "${var.component}"
@@ -27,20 +27,20 @@ module "alb" {
 }
 
 resource "aws_alb_target_group" "default_target_group" {
-  name = replace(replace("${var.env}-default-${var.component}", "/(.{0,32}).*/", "$1"), "/^-+|-+$/", "")
+  name = "${replace(replace("${var.env}-default-${var.component}", "/(.{0,32}).*/", "$1"), "/^-+|-+$/", "")}"
 
   # port will be set dynamically, but for some reason AWS requires a value
   port                 = "31337"
   protocol             = "HTTP"
-  vpc_id               = var.platform_config["vpc"]
-  deregistration_delay = var.default_target_group_deregistration_delay
+  vpc_id               = "${var.platform_config["vpc"]}"
+  deregistration_delay = "${var.default_target_group_deregistration_delay}"
 
   health_check {
-    interval            = var.default_target_group_health_check_interval
-    path                = var.default_target_group_health_check_path
-    timeout             = var.default_target_group_health_check_timeout
-    healthy_threshold   = var.default_target_group_health_check_healthy_threshold
-    unhealthy_threshold = var.default_target_group_health_check_unhealthy_threshold
-    matcher             = var.default_target_group_health_check_matcher
+    interval            = "${var.default_target_group_health_check_interval}"
+    path                = "${var.default_target_group_health_check_path}"
+    timeout             = "${var.default_target_group_health_check_timeout}"
+    healthy_threshold   = "${var.default_target_group_health_check_healthy_threshold}"
+    unhealthy_threshold = "${var.default_target_group_health_check_unhealthy_threshold}"
+    matcher             = "${var.default_target_group_health_check_matcher}"
   }
 }
